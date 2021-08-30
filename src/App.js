75e08340ce6f4d402178e791b3e5e3281c06b2c3
Alvee Akand <alvee.akand@outlook.com>
2021-08-30 17:05:23 -0400

    added ContactCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import GlobalStyles from './components/GlobalStyles';
 import FAQ from './components/FAQ/FAQ';
+import ContactCard from './components/ContactCard/ContactCard';
 
 function App() {
   return (
     <>
     <div className="App">
       <FAQ question="Where would you use Styled Components?" answer="Everywhere!" />
+      {/* <ContactCard
+        avatarSrc="https://cdn.quotesgram.com/small/61/60/913087158-laugh.jpg"
+        name="Mittens"
+        email="meow@gmail.com"
+      /> */}
     </div>
     <GlobalStyles />
     </>
