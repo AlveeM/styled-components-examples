5989e86cfa2688da5450e67549bc31ca5775bc2e
Alvee Akand <alvee.akand@outlook.com>
2021-08-30 16:58:52 -0400

    added examples and global styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import GlobalStyles from './components/GlobalStyles';
+import FAQ from './components/FAQ/FAQ';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FAQ question="Where would you use Styled Components?" answer="Everywhere!" />
     </div>
+    <GlobalStyles />
+    </>
   );
 }
 
